d568f38c2c520ee33adc8e310bcf8f80edc56d04
Dmitry Kudryavtsev <splcell2013@gmail.com>
2022-08-20 19:33:29 +0700

    start project

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+
+const {src, dest, watch, parallel, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'src/'
+        },
+        //notify: false
+    })
+}
+
+
+
+function styles() {
+    return src('src/sass/style.sass')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true}))
+    .pipe(dest('src/css'))
+    .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return src(['node_modules/jquery/dist/jquery.js', 'src/js/main.js'])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('src/js'))
+    .pipe(browserSync.stream())
+}
+
+function images() {
+    return src('src/img/**/*.*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.mozjpeg({quality: 75, progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+    .pipe(dest('dist/img'))
+}
+
+
+function build() {
+    return src(['src/*.html', 'src/css/style.min.css', 'src/js/main.min.js'], {base: 'src'})
+    .pipe(dest('dist'))
+}
+
+function cleanDist() {
+    return del('dist')
+}
+
+function watching() {
+    watch(['src/sass/**/*.sass'], styles);
+    watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
+    watch(['src/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.build = build;
+exports.default = parallel(styles, scripts, browsersync, watching);
